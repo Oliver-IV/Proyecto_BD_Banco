dc2ef37df5abea62a83e29b34c24998eec1a28cb
castr <castr@LAPTOP-HL73NRBN>
2024-02-17 20:20:19 -0700

    Avance HistorialOperaciones

--- src/main/java/org/itson/bdavanzadas/banco/Banco.java
+++ src/main/java/org/itson/bdavanzadas/banco/Banco.java
@@ -1,7 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package org.itson.bdavanzadas.banco;
 
 import java.util.List;
@@ -22,25 +21,23 @@ import org.itson.bdavanzadas.persistencia.conexion.IConexion;
  */
 public class Banco {
 
-    static final Logger logger = Logger.getLogger(Banco.class.getName()) ;
-    
+    static final Logger logger = Logger.getLogger(Banco.class.getName());
+
     public static void main(String[] args) {
-        String cadenaConexion = "jdbc:mysql://localhost/db_banco" ;
-        String user = "root" ;
-        String password = "Itson" ;
-        
-        IConexion conexion = new Conexion(cadenaConexion, user, password) ;
-        IClientesDAO clientesDAO = new ClientesDAO(conexion) ;
-        
-        LoginForm login = new LoginForm(clientesDAO) ;        
-        login.setVisible(true);
+        String cadenaConexion = "jdbc:mysql://localhost/db_banco";
+        String user = "root";
+        String password = "Itson";
+
+        IConexion conexion = new Conexion(cadenaConexion, user, password);
+        IClientesDAO clientesDAO = new ClientesDAO(conexion);
+//        
+//        LoginForm login = new LoginForm(clientesDAO) ;        
+//        login.setVisible(true);
+
 //        TablaSocios tablaSocios = new TablaSocios(sociosDAO) ;
 //        tablaSocios.setVisible(true);
-        
 //        SociosForm sociosForm = new SociosForm(sociosDAO) ;
 //        sociosForm.setVisible(true) ;
-        
-        
 //        ClienteNuevoDTO clienteNuevo = new ClienteNuevoDTO() ;
 //        clienteNuevo.setCp(85150);
 //        clienteNuevo.setNombres("Oliver");
@@ -50,21 +47,39 @@ public class Banco {
 //        clienteNuevo.setCalle("Moises Vazquez");
 //        clienteNuevo.setNumExt(507);
 //        clienteNuevo.setContrasenia("Pera123");
-
-    
-//        
-//        try{
 //            //Transaccion retiro = clientesDAO.agregarRetiroSinCuenta(2, 500) ;
-//            
-//            for (int i = 0; i < 3; i++) {
-//                System.out.println(clientesDAO.obtenerListaCuentas().get(i).getNumCuenta());
+//
+//
+        try {
+            for (int i = 0; i < clientesDAO.obtenerHistorialOperaciones(3, "2024-02-16", "2024-02-17").size(); i++) {
+                
+                System.out.println("numero de cuenta " + clientesDAO.obtenerHistorialOperaciones(3, "2024-02-16", "2024-02-17").get(i).getNumCuentaCliente());
+            }
+        } catch (PersistenciaException ex) {
+            logger.log(Level.SEVERE, null, ex);
+        }
+////            
+////            //logger.log(Level.INFO, retiro.toString());
+////            //List<Cliente> listaSocios = clientesDAO.obtenerListaClientes();
+////            //listaSocios.forEach(socio -> System.out.println(socio));
+
+//        
+//        try {
+//            for (int i = 0; i < 5; i++) {
+//                List<Transaccion> historialOperaciones = clientesDAO.obtenerHistorialOperaciones(i, "2024-02-16", "2024-02-17");
+//
+//                // Verifica si la lista no está vacía antes de intentar acceder a los elementos
+//                if (!historialOperaciones.isEmpty()) {
+//                    System.out.println("Historial de operaciones para el cliente " + i + ":");
+//                    for (Transaccion transaccion : historialOperaciones) {
+//                        System.out.println("ID: " + transaccion.getId());
+//                    }
+//                } else {
+//                    System.out.println("No hay historial de operaciones para el cliente " + i);
+//                }
 //            }
-//            
-//            //logger.log(Level.INFO, retiro.toString());
-//            //List<Cliente> listaSocios = clientesDAO.obtenerListaClientes();
-//            //listaSocios.forEach(socio -> System.out.println(socio));
 //        } catch (PersistenciaException ex) {
-//            logger.log(Level.SEVERE, null, ex) ;
+//            logger.log(Level.SEVERE, null, ex);
 //        }
     }
 }

e65f8eaeecb4938432bc8c2ed00a2714991e663b
castr <castr@LAPTOP-HL73NRBN>
2024-02-19 19:16:02 -0700

    Avance JCalendar + Complementos Oliver

--- src/main/java/org/itson/bdavanzadas/banco/MenuPrincipalForm.java
+++ src/main/java/org/itson/bdavanzadas/banco/MenuPrincipalForm.java
@@ -6,6 +6,7 @@ package org.itson.bdavanzadas.banco;
 
 import java.util.LinkedList;
 import java.util.List;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JOptionPane;
 import org.itson.bdavanzadas.bancodominio.Cliente;
 import org.itson.bdavanzadas.bancodominio.Cuenta;
@@ -19,51 +20,84 @@ import org.itson.bdavanzadas.bancopersistencia.excepciones.PersistenciaException
  */
 public class MenuPrincipalForm extends javax.swing.JFrame {
 
-    IClientesDAO clientesDAO ;
-    List<Cuenta> cuentasCliente ;
-    Cliente cliente ;
-    
+    IClientesDAO clientesDAO;
+    List<Cuenta> cuentasCliente;
+    Cliente cliente;
+
     /**
      * Creates new form MenuPrincipalForm
      */
     public MenuPrincipalForm(IClientesDAO clientesDAO, Cliente cliente) {
         initComponents();
-        this.clientesDAO = clientesDAO ;
-        this.cliente = cliente ;
-        cuentasCliente = new LinkedList() ;
-        obtenerCuentas() ;
-        asignarSaldo() ;
+        this.clientesDAO = clientesDAO;
+        this.cliente = cliente;
+        cuentasCliente = new LinkedList();
+        obtenerCuentas();
+        setComboBoxModel();
+        asignarSaldo();
         lblInicio.setText(lblInicio.getText() + " " + cliente.getNombres() + "!");
     }
 
     public void obtenerCuentas() {
-        long idCliente = cliente.getId() ;
+        long idCliente = cliente.getId();
         try {
             List<Cuenta> listaCuentas = clientesDAO.obtenerListaCuentas();
             for (int i = 0; i < listaCuentas.size(); i++) {
-                long idBuscar = listaCuentas.get(i).getIdCliente() ;
-                
+                long idBuscar = listaCuentas.get(i).getIdCliente();
+
                 if (idCliente == idBuscar) {
-                    cuentasCliente.add(listaCuentas.get(i)) ;
+                    cuentasCliente.add(listaCuentas.get(i));
                 }
-                
+
             }
         } catch (PersistenciaException ex) {
             JOptionPane.showMessageDialog(this, ex.getMessage(), "Error al obtener cuentas", JOptionPane.ERROR_MESSAGE);
         }
     }
-    
+
     public void asignarSaldo() {
-        float saldo = 0 ;
-        for (int i = 0; i < cuentasCliente.size(); i++) {
-            saldo = saldo + cuentasCliente.get(i).getSaldo() ;
-        }
-        txtSaldo.setText(String.valueOf(saldo));
+        txtSaldo.setText(String.valueOf(obtenerCuentaSelec().getSaldo()));
         txtSaldo.setEditable(false);
     }
-    
-    
-    
+
+    public void setComboBoxModel() {
+        String[] arregloCuentas = new String[cuentasCliente.size()];
+        try {
+            for (int i = 0; i < cuentasCliente.size(); i++) {
+                arregloCuentas[i] = "Numero de Cuenta: " + String.valueOf(cuentasCliente.get(i).getNumCuenta());
+            }
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+
+        DefaultComboBoxModel<String> modeloComboBox = new DefaultComboBoxModel<>(arregloCuentas);
+        comboBoxCuentas.setModel(modeloComboBox);
+    }
+
+    public Cuenta obtenerCuentaSelec() {
+        Cuenta cuentaSelec = null;
+        for (int i = 0; i < cuentasCliente.size(); i++) {
+            if (cuentasCliente.get(i).getNumCuenta() == obtenerNumCuentaSelec()) {
+                cuentaSelec = cuentasCliente.get(i);
+            }
+        }
+
+        return cuentaSelec;
+    }
+
+    public long obtenerNumCuentaSelec() {
+        String textoSaldo = comboBoxCuentas.getSelectedItem().toString();
+        String[] numeros = textoSaldo.split("\\D+"); // Divide la cadena en partes que no son números
+        for (String numero : numeros) {
+            if (!numero.isEmpty()) { // Para evitar cadenas vacías
+                int num = Integer.parseInt(numero);
+                return num;
+            }
+        }
+        return -1;
+
+    }
+
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -80,6 +114,7 @@ public class MenuPrincipalForm extends javax.swing.JFrame {
         btnRetiroSinCuenta = new javax.swing.JButton();
         btnHistorialOp = new javax.swing.JButton();
         btnPerfil = new javax.swing.JButton();
+        comboBoxCuentas = new javax.swing.JComboBox<>();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -115,6 +150,8 @@ public class MenuPrincipalForm extends javax.swing.JFrame {
             }
         });
 
+        comboBoxCuentas.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -139,9 +176,12 @@ public class MenuPrincipalForm extends javax.swing.JFrame {
                         .addComponent(lblInicio, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(92, 92, 92)
-                        .addComponent(lblSaldo)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(txtSaldo, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(comboBoxCuentas, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(lblSaldo)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(txtSaldo, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE)))))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -149,7 +189,9 @@ public class MenuPrincipalForm extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(42, 42, 42)
                 .addComponent(lblInicio)
-                .addGap(37, 37, 37)
+                .addGap(5, 5, 5)
+                .addComponent(comboBoxCuentas, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(lblSaldo)
                     .addComponent(txtSaldo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -168,25 +210,26 @@ public class MenuPrincipalForm extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnPerfilActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPerfilActionPerformed
-        JOptionPane.showMessageDialog(this, "Esta función está en desarrollo actualmente", 
-                            "Opcion en Desarrollo", JOptionPane.INFORMATION_MESSAGE);
+        dispose();
+        PerfilForm perfil = new PerfilForm(clientesDAO, cliente);
+        perfil.setVisible(true);
     }//GEN-LAST:event_btnPerfilActionPerformed
 
     private void btnHistorialOpActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnHistorialOpActionPerformed
-        HistorialOperacionesForm historialOperaciones = new HistorialOperacionesForm(clientesDAO, 3);
+        HistorialOperacionesForm historialOperaciones = new HistorialOperacionesForm(clientesDAO, obtenerNumCuentaSelec());
         historialOperaciones.setVisible(true);
 //        JOptionPane.showMessageDialog(this, "Esta función está en desarrollo actualmente", 
 //                            "Opcion en Desarrollo", JOptionPane.INFORMATION_MESSAGE);
     }//GEN-LAST:event_btnHistorialOpActionPerformed
 
     private void btnTransferirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnTransferirActionPerformed
-        JOptionPane.showMessageDialog(this, "Esta función está en desarrollo actualmente", 
-                            "Opcion en Desarrollo", JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(this, "Esta función está en desarrollo actualmente",
+                "Opcion en Desarrollo", JOptionPane.INFORMATION_MESSAGE);
     }//GEN-LAST:event_btnTransferirActionPerformed
 
     private void btnRetiroSinCuentaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRetiroSinCuentaActionPerformed
-       RetiroSinCuentaInForm retiroSinCuenta = new RetiroSinCuentaInForm(clientesDAO, Float.parseFloat(txtSaldo.getText()), cuentasCliente.get(0)) ;
-       retiroSinCuenta.setVisible(true);
+        RetiroSinCuentaInForm retiroSinCuenta = new RetiroSinCuentaInForm(clientesDAO, Float.parseFloat(txtSaldo.getText()), cuentasCliente.get(0));
+        retiroSinCuenta.setVisible(true);
     }//GEN-LAST:event_btnRetiroSinCuentaActionPerformed
 
     /**
@@ -229,6 +272,7 @@ public class MenuPrincipalForm extends javax.swing.JFrame {
     private javax.swing.JButton btnPerfil;
     private javax.swing.JButton btnRetiroSinCuenta;
     private javax.swing.JButton btnTransferir;
+    private javax.swing.JComboBox<String> comboBoxCuentas;
     private javax.swing.JLabel lblInicio;
     private javax.swing.JLabel lblSaldo;
     private javax.swing.JTextField txtSaldo;
